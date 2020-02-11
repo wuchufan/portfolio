19c988f6a6bea3e8b4b29cbebfcf00d13ae6082d
TIEC <TIEC@infos-iMac.local>
2020-02-11 15:59:59 -0500

    finished media query

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,28 @@
  //get sections
 
 const skillSection = document.getElementById('skills');
-const skillPosY = skillSection.getBoundingClientRect().y;
+const skillPosY = skillSection.getBoundingClientRect().y - 250;
 const projectSection = document.getElementById('projects');
-const projectPosY = projectSection.getBoundingClientRect().y;
+const projectPosY = projectSection.getBoundingClientRect().y - 250;
 const contactSection = document.getElementById('contact');
-const contactPosY = contactSection.getBoundingClientRect().y;
+const contactPosY = contactSection.getBoundingClientRect().y - 250;
 const getCard = document.getElementsByClassName('card-box');
 let getCardPosYArr = [];
 //get every project's position
 for (let i = 0; i < getCard.length; i++){
-  getCardPosYArr.push(getCard[i].getBoundingClientRect().y);
+  getCardPosYArr.push(getCard[i].getBoundingClientRect().y - 250);
 }
 
 
 const scrolling = () => {
 
   // Appear skills section
-if(window.pageYOffset >= skillPosY - 480) {
+if(window.pageYOffset >= skillPosY) {
     skillSection.classList.add('section-skills--active');
   }
 
 // Appear projects section
-if (window.pageYOffset >= projectPosY - 150) {
+if (window.pageYOffset >= projectPosY) {
     projectSection.classList.add('section-projects--active');
   }
 
@@ -35,7 +35,7 @@ for (let i = 0; i < getCardPosYArr.length; i++){
 
 
 // Appear contact section
-if (window.pageYOffset >= contactPosY + 550){
+if (window.pageYOffset >= contactPosY){
   contactSection.classList.add('section-contact--active');
 }
 
